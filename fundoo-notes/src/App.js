86ac2baf047ae5693905bbe01e72cf8f750c2678
VengalreddyAkhila <akhila.it1801@gmail.com>
2022-12-24 22:18:50 +0530

    created login and signin page

---
+++ fundoo-notes/src/App.js
@@ -0,0 +1,14 @@
+
+import SignInPage from "./components/signIn/SignInPage";
+import LoginPage from "./components/Login/LoginPage";
+
+function App() {
+  return (
+    <div className="App">
+     {/* <SignInPage/> */}
+     <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
